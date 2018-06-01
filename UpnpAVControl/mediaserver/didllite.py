6f3284de6bd314c7b41ac3a2157a89ca95a564f2
Michael Meier <michael-meier@bluesheep.de>
2018-06-01 22:44:20 +0200

    add first class representing didl.light MusikTracks

--- UpnpAVControl/mediaserver/didllite.py
+++ UpnpAVControl/mediaserver/didllite.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from attr import attrs, attrib
 import lxml.etree
 
@@ -13,6 +14,21 @@ class BaseObject(object):
     title = attrib()
 
 
+@attrs(frozen=True)
+class MusicTrack(object):
+    id = attrib()
+    parentID = attrib()
+    upnpclass = attrib()
+    title = attrib()
+    artist = attrib(default=None)
+    album = attrib(default=None)
+    originalTrackNumber = attrib(default=None)
+    playlist = attrib(default=None)
+    storageMedium = attrib(default=None)
+    contributor = attrib(default=None)
+    date = attrib(default=None)
+
+
 def parse(xmldata):
     tree = lxml.etree.fromstring(xmldata)
     # TODO: validate root element
@@ -21,6 +37,41 @@ def parse(xmldata):
 
 
 def _to_didl_element(xmlElement):
+    upnpclass = xmlElement.findtext('upnp:class', namespaces=nsmap)
+    if upnpclass.startswith('object.item.audioItem.musicTrack'):
+        return _parse_musicitem(xmlElement)
+    else:
+        return _parse_base_object(xmlElement)
+
+
+def _parse_musicitem(xmlElement):
+    id = xmlElement.attrib['id']
+    parentID = xmlElement.attrib['parentID']
+    upnpclass = xmlElement.findtext('upnp:class', namespaces=nsmap)
+    title = xmlElement.findtext('dc:title', namespaces=nsmap)
+    artist = xmlElement.findtext('upnp:artist', namespaces=nsmap)
+    album = xmlElement.findtext('upnp:album', namespaces=nsmap)
+    originalTrackNumber = xmlElement.findtext(
+        'upnp:originalTrackNumber', namespaces=nsmap)
+    playlist = xmlElement.findtext('upnp:playlist', namespaces=nsmap)
+    storageMedium = xmlElement.findtext('upnp:storageMedium', namespaces=nsmap)
+    contributor = xmlElement.findtext('dc:contributor', namespaces=nsmap)
+    date = xmlElement.findtext('dc:date', namespaces=nsmap)
+
+    return MusicTrack(id=id,
+                      parentID=parentID,
+                      upnpclass=upnpclass,
+                      title=title,
+                      artist=artist,
+                      album=album,
+                      originalTrackNumber=originalTrackNumber,
+                      playlist=playlist,
+                      storageMedium=storageMedium,
+                      contributor=contributor,
+                      date=date)
+
+
+def _parse_base_object(xmlElement):
     id = xmlElement.attrib['id']
     parentID = xmlElement.attrib['parentID']
     upnpclass = xmlElement.findtext('upnp:class', namespaces=nsmap)
--- tests/test_didllite.py
+++ tests/test_didllite.py
@@ -1,5 +1,5 @@
+# -*- coding: utf-8 -*-
 from UpnpAVControl.mediaserver import didllite
-
 
 didl1 = """
 <DIDL-Lite xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:sec="http://www.sec.co.kr/" xmlns:dlna="urn:schemas-dlna-org:metadata-1-0/">
@@ -21,6 +21,14 @@ didl1 = """
 </DIDL-Lite>
 """  # noqa
 
+didl_musictrack = """
+<DIDL-Lite xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:sec="http://www.sec.co.kr/" xmlns:dlna="urn:schemas-dlna-org:metadata-1-0/"><item id="26$40260$@40260" parentID="26$40260" restricted="1">
+<dc:title>SomeTitle</dc:title>
+<upnp:class>object.item.audioItem.musicTrack</upnp:class>
+<dc:date>2015-01-01</dc:date><upnp:album>SomeAlbumTitle</upnp:album><upnp:artist>SomeArtist</upnp:artist><dc:creator>SomeArtist</dc:creator><upnp:genre>Alternative</upnp:genre><upnp:author>SomeArtist</upnp:author><upnp:originalTrackNumber>1</upnp:originalTrackNumber><upnp:albumArtURI dlna:profileID="JPEG_TN">http://192.168.0.1:123456/ebdart/40260.jpg</upnp:albumArtURI><res  protocolInfo="http-get:*:audio/mp4:*" size="4161945" bitrate="16000" duration="0:04:08.000" nrAudioChannels="2" sampleFrequency="44100">http://192.168.0.1:123456/NDLNA/40260.m4a</res></item>
+</DIDL-Lite>
+"""  # noqa 501
+
 
 def test_didl_container_parse():
     result = list(didllite.parse(didl1))
@@ -34,3 +42,14 @@ def test_didl_container_parse():
     assert result[2] == didllite.BaseObject(
         id='29$40850$40850', parentID='29$40850', title='Album2',
         upnpclass='object.container.album.musicAlbum')
+
+
+def test_didl_musictrack_parse():
+    result = list(didllite.parse(didl_musictrack))
+    assert len(result) == 1
+    track = result[0]
+    assert track.id == '26$40260$@40260'
+    assert track.parentID == '26$40260'
+    assert track.title == 'SomeTitle'
+    assert track.album == 'SomeAlbumTitle'
+    assert track.upnpclass == 'object.item.audioItem.musicTrack'
