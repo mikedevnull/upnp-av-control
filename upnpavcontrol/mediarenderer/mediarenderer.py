d19005b478b3b065a33e417ea9b94730d97b30e7
Michael Meier <michael-meier@bluesheep.de>
2019-04-21 13:47:51 +0200

    add properties to access media renderer state and services

--- upnpavcontrol/mediarenderer/mediarenderer.py
+++ upnpavcontrol/mediarenderer/mediarenderer.py
@@ -6,4 +6,46 @@ class MediaRenderer(object):
         self._device = upnpclient.Device(location=location)
 
     def __repr__(self):
-        return '<MediaRenderer {}>'.format(self._device.friendly_name)
+        return '<MediaRenderer {}>'.format(self.friendly_name)
+
+    @property
+    def volume(self):
+        response = self.rendering_control.GetVolume(InstanceID=0, Channel='Master')
+        return response['CurrentVolume']
+
+    @volume.setter
+    def volume(self, value):
+        return self.rendering_control.SetVolume(InstanceID=0, Channel='Master', DesiredVolume=value)
+    
+    @property
+    def mute(self):
+        response = self.rendering_control.GetMute(InstanceID=0, Channel='Master')
+        return response['CurrentMute']
+
+    @mute.setter
+    def mute(self, value):
+        self.rendering_control.SetMute(InstanceID=0, Channel='Master', DesiredMute=str(bool(value)))
+
+    @property
+    def rendering_control(self):
+        return self._device.RenderingControl
+
+    @property
+    def av_transport(self):
+        return self._device.avTransport
+
+    @property
+    def connection_manager(self):    
+        return self._device.ConnectionManager
+
+    @property
+    def friendly_name(self):
+        return self._device.friendly_name
+
+    @property
+    def location(self):
+        return self._device.location
+
+    @property
+    def upnp_device(self):
+        return self._device
