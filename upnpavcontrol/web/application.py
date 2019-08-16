e7bf31c8cf2bdfe3c70a08f7bbc2c83a248faa4d
Michael Meier <michael-meier@bluesheep.de>
2019-08-16 08:25:53 +0200

    web-api: add endpoints to get devices, set current player and query current player state

--- upnpavcontrol/web/application.py
+++ upnpavcontrol/web/application.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+import logging
 
 app = FastAPI()
 
 _av_control_point_instance = None
 
 
+class RendererDevice(BaseModel):
+    udn: str
+
+
+class PlaybackInfo(BaseModel):
+    player: str = None
+    volume: int = None
+
+
 def av_control_point():
+    logging.info('av control point dep requested')
     return _av_control_point_instance
 
 
@@ -13,8 +25,21 @@ def _format_device(device):
     return {'name': device.upnp_device.friendly_name, 'udn': device.udn}
 
 
-@app.get('/devices')
+@app.get('/player/devices')
 def device_list(av_cp=Depends(av_control_point)):
-    servers = [_format_device(s) for s in av_cp.media_servers]
-    renderer = [_format_device(s) for s in av_cp.media_renderer]
-    return {'data': {'media_server': servers, 'media_renderer': renderer}}
+    return {'data': [_format_device(s) for s in av_cp.mediarenderers]}
+
+
+@app.put('/player/device')
+def set_active_player(device: RendererDevice, av_cp=Depends(av_control_point)):
+    logging.info('Select device: %s', device.udn)
+    av_cp.set_renderer(device.udn)
+
+
+@app.get('/player', response_model=PlaybackInfo)
+async def current_playback_info(av_cp=Depends(av_control_point)):
+    if av_cp.mediarenderer is None:
+        return {'player': None}
+    else:
+        volume = await av_cp.mediarenderer.get_volume()
+        return {'player': av_cp.mediarenderer.udn, 'volume': volume}
--- upnpavcontrol/web/run.py
+++ upnpavcontrol/web/run.py
@@ -5,7 +5,12 @@ def run_web_api(av_control_point):
     from .application import app as web_app
     from . import application
     application._av_control_point_instance = av_control_point
-    config = uvicorn.Config(web_app, host='127.0.0.1', port=8000)
+
+    config = uvicorn.Config(web_app,
+                            debug=True,
+                            reload=True,
+                            host='127.0.0.1',
+                            port=8000)
     server = uvicorn.Server(config)
 
     return server.serve()
