073f8c8168ad23384886c3e3db1279ef2345d510
Michael Meier <michael-meier@bluesheep.de>
2019-12-31 00:38:41 +0100

    disable uvicorn log config, which used to overwrite our logging settings
    it was particular anyoing that uvicorn strips the log module name within its default config

--- upnpavcontrol/web/run.py
+++ upnpavcontrol/web/run.py
@@ -5,7 +5,7 @@ def run_web_api(av_control_point):
     from .application import app as web_app
     web_app.av_control_point = av_control_point
 
-    config = uvicorn.Config(web_app, debug=True, reload=True, host='127.0.0.1', port=8000)
+    config = uvicorn.Config(web_app, log_config=None, debug=True, reload=True, host='127.0.0.1', port=8000)
     server = uvicorn.Server(config)
 
     return server.serve()
