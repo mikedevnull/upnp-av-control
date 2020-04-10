bd5768e0d09998b4fd9dae1454d834299b0e9469
Michael Meier <michael-meier@bluesheep.de>
2020-04-10 12:27:01 +0200

    move player api endpoints to dedicated submodule

--- upnpavcontrol/web/api/api.py
+++ upnpavcontrol/web/api/api.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from . import library
+from . import player
 
 router = APIRouter()
 router.include_router(library.router, prefix='/library')
+router.include_router(player.router, prefix='/player')
--- upnpavcontrol/web/application.py
+++ upnpavcontrol/web/application.py
@@ -1,7 +1,5 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, validator
+from fastapi import FastAPI
 from starlette.websockets import WebSocket, WebSocketDisconnect
-import logging
 from upnpavcontrol.core import AVControlPoint
 from .broadcast_event_bus import BroadcastEventBus
 from .models import DiscoveryEvent
@@ -35,60 +33,6 @@ class AVControlPointAPI(FastAPI):
 app = AVControlPointAPI()
 
 app.include_router(api.router)
-
-
-class RendererDevice(BaseModel):
-    udn: str
-
-
-class PlaybackInfo(BaseModel):
-    player: str = None
-    volume: int = None
-
-
-def _format_device(device):
-    return {'name': device.upnp_device.friendly_name, 'udn': device.udn}
-
-
-@app.get('/player/devices')
-def device_list():
-    return {'data': [_format_device(s) for s in app.av_control_point.mediarenderers]}
-
-
-@app.put('/player/device')
-async def set_active_player(device: RendererDevice):
-    try:
-        logging.info('Select device: %s', device.udn)
-        await app.av_control_point.set_renderer(device.udn)
-    except KeyError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-
-class Volume(BaseModel):
-    volume_percent: int
-
-    @validator('volume_percent')
-    def volume_percent_range(cls, v):
-        if v < 0 or v > 100:
-            raise ValueError('Volume out of range (0 <= volume <= 100)')
-        return v
-
-
-@app.put('/player/volume', status_code=204)
-async def set_player_volume(volume: Volume):
-    if app.av_control_point.mediarenderer is None:
-        raise HTTPException(status_code=404)
-    await app.av_control_point.mediarenderer.set_volume(volume.volume_percent)
-    return ''
-
-
-@app.get('/player', response_model=PlaybackInfo)
-async def current_playback_info():
-    if app.av_control_point.mediarenderer is None:
-        return {'player': None}
-    else:
-        volume = await app.av_control_point.mediarenderer.get_volume()
-        return {'player': app.av_control_point.mediarenderer.udn, 'volume': volume}
 
 
 @app.websocket('/ws/events')
