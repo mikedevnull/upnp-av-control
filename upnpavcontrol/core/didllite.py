0f8f1ddb9ba1b71271ad8261c240f1cc5adb225d
Michael Meier <michael-meier@bluesheep.de>
2020-04-11 16:17:18 +0200

    exchange use of python-didl-lite with implementation based on pydantic
    it makes more sense to directly convert didl xml to data types can be easily consumed by the web endpoints.
    Before that, we converted everything to python-didl-lite structures before doing another conversion
    for the web endpoint anyway.

--- upnpavcontrol/core/didllite.py
+++ upnpavcontrol/core/didllite.py
@@ -1,91 +1,132 @@
 # -*- coding: utf-8 -*-
-from attr import attrs, attrib
-import lxml.etree
-
-nsmap = {'upnp': 'urn:schemas-upnp-org:metadata-1-0/upnp/', 'dc': 'http://purl.org/dc/elements/1.1/'}
-
-
-@attrs(frozen=True)
-class BaseObject(object):
-    id = attrib()
-    parentID = attrib()
-    upnpclass = attrib()
-    title = attrib()
+import defusedxml.ElementTree as etree
+from pydantic import BaseModel
+import typing
 
 
-@attrs(frozen=True)
-class MusicTrack(object):
-    id = attrib()
-    parentID = attrib()
-    upnpclass = attrib()
-    title = attrib()
-    artist = attrib(default=None)
-    album = attrib(default=None)
-    originalTrackNumber = attrib(default=None)
-    playlist = attrib(default=None)
-    storageMedium = attrib(default=None)
-    contributor = attrib(default=None)
-    date = attrib(default=None)
+class DidlObject(BaseModel):
+    id: str
+    parentID: str
+    upnpclass: str
+    title: str
 
 
-def parse(xmldata):
-    tree = lxml.etree.fromstring(xmldata)
+class DidlItem(DidlObject):
+    pass
+
+
+class DidlContainer(DidlObject):
+    pass
+
+
+class MusicTrack(DidlItem):
+    artist: typing.Optional[str]
+    album: typing.Optional[str]
+    originalTrackNumber: typing.Optional[int]
+    playlist: typing.Optional[str]
+    storageMedium: typing.Optional[str]
+    contributor: typing.Optional[str]
+    date: typing.Optional[str]
+
+
+# Not all fields are present in all models, but
+# as their mappings don't overlap (for the subset we're currently supporting)
+# it's ok to use a single mapping structure.
+# Optional and required fields will be enfored by the model itself
+_field_to_didl_mapping = {
+    'id': '@id',
+    'parentID': '@parentID',
+    'upnpclass': 'upnp:class',
+    'title': 'dc:title',
+    'artist': 'upnp:artist',
+    'album': 'upnp:album',
+    'originalTrackNumber': 'upnp:originalTrackNumber',
+    'playlist': 'upnp:playlist',
+    'storageMedium': 'upnp:storageMedium',
+    'contributor': 'dc:contributor',
+    'date': 'dc:date'
+}
+
+_nsmap = {'upnp': 'urn:schemas-upnp-org:metadata-1-0/upnp/', 'dc': 'http://purl.org/dc/elements/1.1/'}
+
+
+class DictAdapter():
+    """
+    Given a field mapping, this class provides a map like interface to
+    an XML element.
+
+    This provides a uniform an configurable way to transform XML elements
+    into python/pydantic dataclasses.
+
+    Adapted from pydantic.utils.GetterDict.
+    """
+    def __init__(self, obj: typing.Any, key_map: typing.Mapping[str, str], namespaces: typing.Mapping[str, str] = None):
+        self._xml = obj
+        self._key_map = key_map
+        self._nsmap = namespaces
+
+    def _get_from_xml(self, key, default=None):
+        mapped_key = self._key_map[key]
+        if mapped_key[0] == '@':
+            return self._xml.attrib.get(mapped_key[1:], default)
+        else:
+            value = self._xml.findtext(mapped_key, namespaces=self._nsmap)
+            if value is None:
+                return default
+            return value
+
+    def __getitem__(self, key: str) -> typing.Any:
+        return self._get_from_xml(key)
+
+    def get(self, key: typing.Any, default: typing.Any = None) -> typing.Any:
+        return self._get_from_xml(key, default)
+
+    def keys(self) -> typing.List[typing.Any]:
+        """
+        Keys of the pseudo dictionary, uses a list not set so order information can be maintained like python
+        dictionaries.
+        """
+        return self._key_map.keys()
+
+    def values(self) -> typing.List[typing.Any]:
+        return [self[k] for k in self]
+
+    def items(self) -> typing.Iterator[typing.Tuple[str, typing.Any]]:
+        for k in self:
+            yield k, self.get(k)
+
+    def __iter__(self) -> typing.Iterator[str]:
+        for key in self._key_map:
+            yield key
+
+    def __len__(self) -> int:
+        return len(self._key_map)
+
+    def __contains__(self, item: typing.Any) -> bool:
+        return item in self.keys()
+
+
+def _create_xml_adapter(xmlElement, field_mapping=_field_to_didl_mapping):
+    return DictAdapter(xmlElement, field_mapping, _nsmap)
+
+
+def from_xml_string(xmldata):
+    tree = etree.fromstring(xmldata)
     # TODO: validate root element
     for child in tree:
         yield _to_didl_element(child)
 
 
 def _to_didl_element(xmlElement):
-    upnpclass = xmlElement.findtext('upnp:class', namespaces=nsmap)
+    upnpclass = xmlElement.findtext('upnp:class', namespaces=_nsmap)
+    # mapping logic should be improved/refactored when more types are supported
     if upnpclass.startswith('object.item.audioItem.musicTrack'):
-        return _parse_musicitem(xmlElement)
+        cls = MusicTrack
+    elif upnpclass.startswith('object.item'):
+        cls = DidlItem
+    elif upnpclass.startswith('object.container'):
+        cls = DidlContainer
     else:
-        return _parse_base_object(xmlElement)
-
-
-@attrs(frozen=True)
-class _DidlEntry(object):
-    name = attrib()
-    key = attrib()
-    required = attrib(type=bool, default=True)
-
-
-def _parse_didl(xmlElement, attributes, elements):
-    data = {}
-    for attribute in attributes:
-        value = xmlElement.attrib[attribute.name]
-        if attribute.required and value is None:
-            raise RuntimeError('required attribute %s not present in DIDL-Lite data' % attribute.name)
-        data[attribute.key] = value
-
-    for element in elements:
-        value = xmlElement.findtext(element.name, namespaces=nsmap)
-        if element.required and value is None:
-            raise RuntimeError('required element %s not present in DIDL-Lite data' % element.name)
-        data[element.key] = value
-
-    return data
-
-
-def _parse_musicitem(xmlElement):
-    data = _parse_didl(xmlElement,
-                       attributes=(_DidlEntry(name='id', key='id'), _DidlEntry(name='parentID', key='parentID')),
-                       elements=(_DidlEntry(name='upnp:class', key='upnpclass'), _DidlEntry(name='dc:title',
-                                                                                            key='title'),
-                                 _DidlEntry(name='upnp:artist', key='artist',
-                                            required=False), _DidlEntry(name='upnp:album', key='album', required=False),
-                                 _DidlEntry(name='upnp:originalTrackNumber', key='originalTrackNumber', required=False),
-                                 _DidlEntry(name='upnp:playlist', key='playlist', required=False),
-                                 _DidlEntry(name='upnp:storageMedium', key='storageMedium', required=False),
-                                 _DidlEntry(name='dc:contributor', key='contributor',
-                                            required=False), _DidlEntry(name='dc:date', key='date', required=False)))
-
-    return MusicTrack(**data)
-
-
-def _parse_base_object(xmlElement):
-    id = xmlElement.attrib['id']
-    parentID = xmlElement.attrib['parentID']
-    upnpclass = xmlElement.findtext('upnp:class', namespaces=nsmap)
-    title = xmlElement.findtext('dc:title', namespaces=nsmap)
-    return BaseObject(id=id, parentID=parentID, upnpclass=upnpclass, title=title)
+        cls = DidlObject
+    adapter = _create_xml_adapter(xmlElement)
+    return cls.parse_obj(adapter)
