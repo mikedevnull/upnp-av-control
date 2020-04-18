4108fcb5766137da14560ace2fa243044b8d0d11
Michael Meier <michael-meier@bluesheep.de>
2020-04-18 14:57:02 +0200

    add command line option to configure host

--- upnpavcontrol/tools/web_control_point.py
+++ upnpavcontrol/tools/web_control_point.py
@@ -7,8 +7,8 @@ import uvicorn
 _logger = logging.getLogger(__name__)
 
 
-def run_web_control_point():
-    config = uvicorn.Config(web_app, log_config=None, debug=True, reload=True, host='127.0.0.1', port=8000)
+def run_web_control_point(host, port):
+    config = uvicorn.Config(web_app, log_config=None, debug=True, reload=True, host=host, port=port)
     server = uvicorn.Server(config)
     server.run()
 
@@ -32,10 +32,12 @@ def main():
         dest="loglevel",
         const=logging.INFO,
     )
+    parser.add_argument('--host', help="Host to bind", type=str, default="127.0.0.1")
+    parser.add_argument('--port', help="Port to listen", type=int, default=8000)
     args = parser.parse_args()
     colorlog.basicConfig(level=args.loglevel, format='%(log_color)s%(levelname)s:%(name)s:%(message)s')
     logging.getLogger('async_upnp_client').setLevel(logging.INFO)
-    run_web_control_point()
+    run_web_control_point(host=args.host, port=args.port)
     logging.info('Successfully shutdown UPnP-AV WebControlPoint')
 
 
