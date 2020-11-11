7674e89753e2218a00cd9bd6386a6eadee318ebc
Michael Meier <michael-meier@bluesheep.de>
2020-11-11 10:54:39 +0100

    switch to vue/cli for frontend development ...
    ... because life's too short to configure webpack manually ...

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,20 +1,10 @@
 module.exports = {
-  verbose: true,
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{js,vue}", "!**/node_modules/**"],
-  coverageReporters: ["lcov", "html", "text-summary"],
-  moduleFileExtensions: [
-    "js",
-    "json",
-    "vue"
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  testMatch: [
+    "<rootDir>/tests/*.spec.(js|jsx|ts|tsx)",
+    "<rootDir>/src/**/tests/*.spec.(js|jsx|ts|tsx)"
   ],
   transform: {
-    "^.+\\.vue$": "vue-jest",
-    "^.+\\.js$": "babel-jest",
     "^.+\\.svg$": "<rootDir>/tests/svgTransform.js"
-  },
-  moduleNameMapper: {
-    "^@/assets/(.*)": "<rootDir>/src/assets/$1",
-    "^@/(.*)$": "<rootDir>/src/$1"
   }
 };
