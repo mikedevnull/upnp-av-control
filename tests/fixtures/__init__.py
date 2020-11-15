8603989ed05443877e2afc9cd78ca9b4e83dfae2
Michael Meier <michael-meier@bluesheep.de>
2020-11-15 01:49:50 +0100

    remove obsolete and overly complex testing fixture code

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -1,17 +1,11 @@
 from .fixtures import webapi_client
-from .fixtures import started_mocked_device_registry
-from .fixtures import mocked_device_registry
 from .fixtures import mocked_renderer_device
 from .fixtures import mocked_server_device
-from .fixtures import mocked_notification_backend
-from .fixtures import testing_av_control_point
+from .fixtures import mocked_av_control_point
 
 __all__ = [
     'webapi_client',
-    'started_mocked_device_registry',
-    'mocked_device_registry',
     'mocked_renderer_device',
     'mocked_server_device',
-    'mocked_notification_backend',
-    'testing_av_control_point',
+    'mocked_av_control_point',
 ]
--- tests/fixtures/fixtures.py
+++ tests/fixtures/fixtures.py
@@ -9,62 +9,40 @@ from . import upnp_device_mocks, upnp_event_mocks
 
 
 @pytest.fixture
-async def mocked_device_registry():
+async def mocked_av_control_point(event_loop):
     registry = discovery.DeviceRegistry(advertisement_listener=TestingAdvertisementListener,
                                         upnp_requester=create_test_requester())
 
-    registry.trigger_renderer_alive = registry._listener.trigger_renderer_alive
-    registry.trigger_renderer_byebye = registry._listener.trigger_renderer_byebye
-    registry.trigger_server_alive = registry._listener.trigger_server_alive
-    registry.trigger_printer_alive = registry._listener.trigger_printer_alive
-
-    return registry
-
-
-@pytest.fixture
-async def started_mocked_device_registry(mocked_device_registry):
-    await mocked_device_registry.async_start()
-    yield mocked_device_registry
-    await mocked_device_registry.async_stop()
-
-
-@pytest.fixture
-def mocked_notification_backend(event_loop):
-    return upnp_event_mocks.create_test_notification_backend()
-
-
-@pytest.fixture
-async def testing_av_control_point(event_loop, mocked_device_registry, mocked_notification_backend):
     from upnpavcontrol.core import AVControlPoint
-    test_control_point = AVControlPoint(device_registry=mocked_device_registry,
-                                        notifcation_backend=mocked_notification_backend)
+    test_control_point = AVControlPoint(device_registry=registry,
+                                        notifcation_backend=upnp_event_mocks.create_test_notification_backend())
 
     yield test_control_point
 
 
 @pytest.fixture
-async def webapi_client(event_loop, testing_av_control_point):
+async def webapi_client(event_loop, mocked_av_control_point):
     from upnpavcontrol.web import application
     from async_asgi_testclient import TestClient
-    application.app.av_control_point = testing_av_control_point
+    application.app.av_control_point = mocked_av_control_point
 
     async with TestClient(application.app) as test_client:
         yield test_client
 
 
 @pytest.fixture
-async def mocked_renderer_device(testing_av_control_point):
+async def mocked_renderer_device(mocked_av_control_point):
     renderer = MediaRenderer(upnp_device_mocks.UpnpMediaRendererDevice())
     mockedRendererEntry = DeviceEntry(device=renderer, device_type="urn:schemas-upnp-org:device:MediaRenderer:1")
     udn = mockedRendererEntry.device.udn
-    testing_av_control_point._devices._av_devices[udn] = mockedRendererEntry
+    mocked_av_control_point._devices._av_devices[udn] = mockedRendererEntry
     return mockedRendererEntry.device
 
 
 @pytest.fixture
-async def mocked_server_device(testing_av_control_point):
+async def mocked_server_device(mocked_av_control_point):
     server = MediaServer(upnp_device_mocks.UpnpMediaServerDevice())
     mockedServerEntry = DeviceEntry(device=server, device_type="urn:schemas-upnp-org:device:MediaServer:1")
     udn = mockedServerEntry.device.udn
-    testing_av_control_point._devices._av_devices[udn] = mockedServerEntry
+    mocked_av_control_point._devices._av_devices[udn] = mockedServerEntry
     return mockedServerEntry.device
