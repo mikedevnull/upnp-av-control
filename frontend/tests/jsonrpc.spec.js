7b2a1bd504e035e195bc1509905e3f0101d6af0c
Michael Meier <michael-meier@bluesheep.de>
2021-01-10 00:49:09 +0100

    add jsonrpc client to issue calls and receive notifications

---
+++ frontend/tests/jsonrpc.spec.js
@@ -0,0 +1,79 @@
+import JsonRPCClient from "@/jsonrpc";
+
+describe("JsonRPCClient notifications", () => {
+  it("invokes registered callback on notification", () => {
+    const client = new JsonRPCClient();
+    const callback = jest.fn();
+    client.on("some_notification", callback);
+    client.handleMessage('{"jsonrpc": "2.0", "method":"some_notification"}');
+    expect(callback).toHaveBeenCalled();
+  });
+
+  it("passes list arguments to callbacks", () => {
+    const client = new JsonRPCClient();
+    const callback = jest.fn();
+    client.on("some_notification", callback);
+    client.handleMessage(
+      '{"jsonrpc": "2.0", "method":"some_notification", "params":[1,2]}'
+    );
+    expect(callback).toHaveBeenCalledWith(1, 2);
+  });
+
+  it("passes object argument to callbacks", () => {
+    const client = new JsonRPCClient();
+    const callback = jest.fn();
+    client.on("some_notification", callback);
+    client.handleMessage(
+      '{"jsonrpc": "2.0", "method":"some_notification", "params":{"foo": "bar", "baz": 42}}'
+    );
+    expect(callback).toHaveBeenCalledWith({ foo: "bar", baz: 42 });
+  });
+});
+
+describe("JsonRPCClient calls", () => {
+  let lastCall;
+  const streamer = payload => {
+    lastCall = JSON.parse(payload);
+  };
+  it("invokes function and returns result", () => {
+    const client = new JsonRPCClient();
+    client.streamTo = streamer;
+    expect.assertions(4);
+    const result1 = client.call("foo", 12);
+    expect(lastCall.method).toBe("foo");
+    const requestid1 = lastCall.id;
+    const result2 = client.call("bar");
+    expect(lastCall.method).toBe("bar");
+    const requestid2 = lastCall.id;
+    // simulate responses in different order
+    client.handleMessage(
+      JSON.stringify({ jsonrpc: "2.0", result: 12, id: requestid2 })
+    );
+    client.handleMessage(
+      JSON.stringify({ jsonrpc: "2.0", result: 42, id: requestid1 })
+    );
+    expect(result1).resolves.toEqual(42);
+    expect(result2).resolves.toEqual(12);
+  });
+
+  it("rejects promise on jsonrpc error", () => {
+    const client = new JsonRPCClient();
+    client.streamTo = streamer;
+    expect.assertions(2);
+    const result1 = client.call("foo", 12);
+    expect(lastCall.method).toBe("foo");
+    const requestid1 = lastCall.id;
+    client.handleMessage(
+      JSON.stringify({
+        jsonrpc: "2.0",
+        error: { code: -321, message: "uhh, something went wrong" },
+        id: requestid1
+      })
+    );
+
+    return expect(result1).rejects.toStrictEqual({
+      code: -321,
+      message: "uhh, something went wrong"
+    });
+  });
+});
