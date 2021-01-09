658f59cb6ea3823ed9e71c1ec0208fb7aaa43388
Michael Meier <michael-meier@bluesheep.de>
2021-01-10 00:50:07 +0100

    adapt frontend websocket event bus to jsonrpc based api

--- frontend/tests/controlpoint-event-bus.spec.js
+++ frontend/tests/controlpoint-event-bus.spec.js
@@ -14,11 +14,15 @@ describe("UpnpEventBus", () => {
   beforeEach(() => {
     actions = {
       updateAvailableRenderers: jest.fn(),
-      updateAvailableServers: jest.fn(),
-      updatePlaybackInfo: jest.fn()
+      updateAvailableServers: jest.fn()
     };
     store = new Vuex.Store({ actions });
-    socketMock = { url: undefined, onmessage: undefined, close: jest.fn() };
+    socketMock = {
+      url: undefined,
+      onmessage: undefined,
+      close: jest.fn(),
+      send: jest.fn()
+    };
     createMockWebsocket = jest.fn(url => {
       socketMock.url = url;
       return socketMock;
@@ -35,20 +39,40 @@ describe("UpnpEventBus", () => {
     const bus = new ControlPointEventBus(store, createMockWebsocket);
     bus.run();
     expect(bus.state).toBe("closed");
-    socketMock.onmessage({ data: '{ "version": "0.0.1" }' });
+    socketMock.onmessage({
+      data:
+        '{ "jsonrpc": "2.0", "method": "initialize", "params":{"version": "0.2.0" }}'
+    });
     expect(bus.state).toBe("connected");
+  });
+
+  it("should request discovery notifications", () => {
+    const bus = new ControlPointEventBus(store, createMockWebsocket);
+    bus.run();
+    socketMock.onmessage({
+      data:
+        '{ "jsonrpc": "2.0", "method": "initialize", "params":{"version": "0.2.0" }}'
+    });
+    expect(socketMock.send).toHaveBeenCalled();
+    const requestdata = JSON.parse(socketMock.send.mock.calls[0][0]);
+    expect(requestdata.jsonrpc).toBe("2.0");
+    expect(requestdata.method).toBe("subscribe");
+    expect(requestdata.params.category).toBe("discovery");
   });
 
   it("should reject wrong versions on initial handsake", () => {
     const bus = new ControlPointEventBus(store, createMockWebsocket);
     bus.run();
     expect(bus.state).toBe("closed");
-    socketMock.onmessage({ data: '{ "version": "0.0.2" }' });
+    socketMock.onmessage({
+      data:
+        '{ "jsonrpc": "2.0", "method": "initialize", "params":{"version": "0.1.0" }}'
+    });
     expect(bus.state).toBe("closed");
     expect(socketMock.close).toHaveBeenCalled();
   });
 
-  it("should wrong payload data on initial handsake", () => {
+  it("should handle wrong payload data on initial handsake", () => {
     const bus = new ControlPointEventBus(store, createMockWebsocket);
     bus.run();
     expect(bus.state).toBe("closed");
@@ -62,32 +86,34 @@ describe("UpnpEventBus", () => {
     bus.run();
     expect(actions.updateAvailableRenderers).toHaveBeenCalled();
     expect(actions.updateAvailableServers).toHaveBeenCalled();
-    expect(actions.updatePlaybackInfo).toHaveBeenCalled();
   });
 
   it("should trigger a data update on new device events", () => {
     const bus = new ControlPointEventBus(store, createMockWebsocket);
     bus.run();
     // handshake
-    socketMock.onmessage({ data: '{ "version": "0.0.1" }' });
+    socketMock.onmessage({
+      data:
+        '{ "jsonrpc": "2.0", "method": "initialize", "params":{"version": "0.2.0" }}'
+    });
     // actual event
-    socketMock.onmessage({ data: '{"event_type": "NEW_DEVICE"}' });
+    socketMock.onmessage({
+      data:
+        '{ "jsonrpc": "2.0", "method": "new_device", "params":{"udn": "1234", "device_type": "MediaRenderer" }}'
+    });
     // once on run(), once on event
     expect(actions.updateAvailableRenderers).toHaveBeenCalledTimes(2);
-    expect(actions.updateAvailableServers).toHaveBeenCalledTimes(2);
-    expect(actions.updatePlaybackInfo).toHaveBeenCalledTimes(2);
   });
 
   it("should trigger a data update on new device losts", () => {
     const bus = new ControlPointEventBus(store, createMockWebsocket);
     bus.run();
     // handshake
-    socketMock.onmessage({ data: '{ "version": "0.0.1" }' });
-    // actual event
-    socketMock.onmessage({ data: '{"event_type": "DEVICE_LOST"}' });
+    socketMock.onmessage({
+      data:
+        '{ "jsonrpc": "2.0", "method": "device_lost", "params":{"udn": "1234", "device_type": "MediaRenderer" }}'
+    }); // actual event
     // once on run(), once on event
     expect(actions.updateAvailableRenderers).toHaveBeenCalledTimes(2);
-    expect(actions.updateAvailableServers).toHaveBeenCalledTimes(2);
-    expect(actions.updatePlaybackInfo).toHaveBeenCalledTimes(2);
   });
 });
