4f0c528979d5774c723a49f202310a9b75aec2a6
Michael Meier <michael-meier@bluesheep.de>
2021-03-13 09:35:12 +0100

    add ability to simulate ssdp events to testing advertisement listener

--- tests/testsupport/null_advertisement_listener.py
+++ tests/testsupport/null_advertisement_listener.py
@@ -1,4 +1,6 @@
 from upnpavcontrol.core.discovery.advertisement import AdvertisementListenerInterface
+from upnpavcontrol.core.discovery.registry import MediaDeviceDiscoveryEvent
+from upnpavcontrol.core.discovery.advertisement import _DeviceAdvertisementHandler
 
 
 class NullAdvertisementListener(AdvertisementListenerInterface):
@@ -7,7 +9,19 @@ class NullAdvertisementListener(AdvertisementListenerInterface):
     methods to manually trigger discovery events for testing
     """
     def __init__(self, event_queue):
-        pass
+        self._handler = _DeviceAdvertisementHandler(event_queue)
+
+    async def simulate(self, data):
+        assert data['NTS'] in ('ssdp:alive', 'ssdp:update', 'ssdp::byebye')
+        if data['NTS'] == 'ssdp:alive':
+            await self._handler.on_alive(data)
+        elif data['NTS'] == 'ssdp:update':
+            await self._handler.on_update(data)
+        elif data['NTS'] == 'ssdp:byebye':
+            await self._handler.on_bybye(data)
+
+    async def trigger_alive(self, data):
+        await self._handler.on_alive(data)
 
     async def async_start(self):
         pass
