bea92d6810a384974f0cc3ae29c6b13d1ea82f7f
Michael Meier <michael-meier@bluesheep.de>
2021-03-13 09:31:53 +0100

    fixup testsupport import handling

--- tests/testsupport/__init__.py
+++ tests/testsupport/__init__.py
@@ -1,4 +1,6 @@
 from .asyncmock_stub import AsyncMock
 from .null_advertisement_listener import NullAdvertisementListener
+from .upnp_test_requester import UpnpTestRequester
+from .notification_test_endpoint import NotificationTestEndpoint
 
-__all__ = ['AsyncMock', 'NullAdvertisementListener']
+__all__ = ['AsyncMock', 'NullAdvertisementListener', 'UpnpTestRequester', 'NotificationTestEndpoint']
