21141387b24c33f5f92ce1f1e9bc1f6231275a26
Michael Meier <michael-meier@bluesheep.de>
2021-03-14 23:15:02 +0100

    add timeout to test jsonrpc connection
    avoid waiting too long for tests to fail

--- tests/bdd/json_rpc_connection.py
+++ tests/bdd/json_rpc_connection.py
@@ -1,4 +1,5 @@
 from upnpavcontrol.web import json_rpc
+import asyncio
 
 
 class JsonRPCTestConnection(object):
@@ -7,6 +8,7 @@ class JsonRPCTestConnection(object):
         self._nextId = 1
         self.api_version = None
         self.state = 'disconnected'
+        self.timeout = 5
 
     async def __aenter__(self):
         initEvent = await self.wait_for_notification()
@@ -22,12 +24,12 @@ class JsonRPCTestConnection(object):
         request = json_rpc.JsonRPCRequest(id=self._nextId, method=method, params=params, jsonrpc='2.0')
         self._nextId = +1
         await self._websocket.send_text(request.json())
-        raw_reply = await self._websocket.receive_json()
+        raw_reply = await asyncio.wait_for(self._websocket.receive_json(), timeout=self.timeout)
         reply = json_rpc.JsonRPCResponse(**raw_reply)
         assert reply.id == request.id
         return reply.result
 
     async def wait_for_notification(self):
-        raw_reply = await self._websocket.receive_json()
+        raw_reply = await asyncio.wait_for(self._websocket.receive_json(), timeout=self.timeout)
         reply = json_rpc.JsonRPCNotification(**raw_reply)
         return reply
