2220d75297ae77cb79b49927b9260326dfe47d93
Michael Meier <michael-meier@bluesheep.de>
2021-03-22 02:01:43 +0100

    add webapi volume changing tests

--- tests/bdd/test_media_renderer_api.py
+++ tests/bdd/test_media_renderer_api.py
@@ -24,9 +24,42 @@ async def discovery_events_subscribed(test_context, event_bus_connection, render
 
 @when(parsers.cfparse('the playback volume of AcmeRenderer changes to {volume:d}'))
 @sync
-async def unsubscribe_discovery_events(test_context, event_bus_connection, volume):
+async def external_volume_change(test_context, event_bus_connection, volume):
     device = test_context.get_device('AcmeRenderer')
     await device.service('urn:schemas-upnp-org:service:RenderingControl:1')._set_volume(volume)
+
+
+@when(parsers.cfparse('the client requests to change the volume of {device_name} to {volume:d}'))
+@sync
+async def change_renderer_volume(test_context, device_name, webclient, volume):
+    if device_name == "an unkown device":
+        udn = 'fake-udn-no-device-has-this'
+    else:
+        device = test_context.get_device(device_name)
+        udn = device.udn
+    test_context.last_response = await webclient.put(f'/player/{udn}/volume', json={'volume_percent': volume})
+
+
+@when(parsers.cfparse('the client requests the volume of {device_name}'))
+@sync
+async def query_renderer_volume(test_context, device_name, webclient):
+    if device_name == "an unkown device":
+        udn = 'fake-udn-no-device-has-this'
+    else:
+        device = test_context.get_device(device_name)
+        udn = device.udn
+    test_context.last_response = await webclient.get(f'/player/{udn}/volume')
+
+
+@then(parsers.cfparse('the volume reported by the API of {device_name} is {volume:d}'))
+@sync
+async def check_renderer_volume_with_webapi(test_context, device_name, webclient, volume):
+    device = test_context.get_device(device_name)
+    udn = device.udn
+    response = await webclient.get(f'/player/{udn}/volume')
+    test_context.last_response = response
+    assert response.status_code == 200
+    assert response.json() == {'volume_percent': volume}
 
 
 @then(parsers.cfparse('the client will be notified about the new volume {volume:d}'))
@@ -36,3 +69,8 @@ async def check_device_notification(event_bus_connection, volume):
     assert event.method == 'playbackinfo'
     info = event.params['playbackinfo']
     assert info['volume_percent'] == volume
+
+
+@then('an error has been reported')
+def check_last_webclient_response(test_context):
+    assert test_context.last_response.status_code >= 300
