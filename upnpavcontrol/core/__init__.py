57e3b50258d22aae81abef400547f6c654d07633
Michael Meier <michael-meier@bluesheep.de>
2021-04-30 22:16:08 +0200

    add access to protocol info of mediarenderers

--- upnpavcontrol/core/__init__.py
+++ upnpavcontrol/core/__init__.py
@@ -1,5 +1,9 @@
 from .mediaserver import MediaServer
+from .mediarenderer import MediaRenderer
 from .application import AVControlPoint
 from .playback import ProtocolInfoEntry, parse_protocol_infos, protocol_info_matches
 
-__all__ = ['MediaServer', 'AVControlPoint', 'ProtocolInfoEntry', 'parse_protocol_infos', 'protocol_info_matches']
+__all__ = [
+    'MediaServer', 'MediaRenderer', 'AVControlPoint', 'ProtocolInfoEntry', 'parse_protocol_infos',
+    'protocol_info_matches'
+]
