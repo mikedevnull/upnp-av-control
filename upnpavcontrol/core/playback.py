a8dbc844897ddafb4ed137f4569b4e0e614942d3
Michael Meier <michael-meier@bluesheep.de>
2021-04-30 22:33:16 +0200

    minimal, incomplete implementation to start playback
    especially, connection initialization only works for devices that do not implement the optional PrepareForConnection method

--- upnpavcontrol/core/playback.py
+++ upnpavcontrol/core/playback.py
@@ -1,8 +1,22 @@
 from dataclasses import dataclass, field
-import csv
-import io
 import typing
 from . import didllite
+
+
+@dataclass
+class PreparedConnection(object):
+    connection_id: int
+    av_transport_id: int
+    rcs_id: int
+
+
+def prepare_for_connection(dmr: 'MediaRenderer', dms: 'MediaServer') -> PreparedConnection:  # noqa F812
+    # this is a stub, needs a proper implementation for devices that support PrepareForConnection
+    if dmr.connection_manager.has_action('PrepareForConnection'):
+        raise NotImplementedError('prepare for connection handling not yet implemented')
+    if dms.connection_manager.has_action('PrepareForConnection'):
+        raise NotImplementedError('prepare for connection handling not yet implemented')
+    return PreparedConnection(connection_id=0, av_transport_id=0, rcs_id=0)
 
 
 @dataclass
@@ -42,7 +56,8 @@ def parse_protocol_infos(data):
 
 
 def protocol_info_matches(lhs: ProtocolInfoEntry, rhs: ProtocolInfoEntry):
-    return lhs is not None and rhs is not None and lhs.protocol == rhs.protocol and lhs.content_format.mimetype == rhs.content_format.mimetype
+    return lhs is not None and rhs is not None and \
+        lhs.protocol == rhs.protocol and lhs.content_format.mimetype == rhs.content_format.mimetype
 
 
 def match_resources(object_resources: typing.Sequence[didllite.Resource],
@@ -55,3 +70,20 @@ def match_resources(object_resources: typing.Sequence[didllite.Resource],
                 matched_resources.append(object_resource)
                 break
     return matched_resources
+
+
+async def play(dms: 'MediaServer', object_id: str, dmr: 'MediaRenderer'):  # noqa F812
+    didl = await dms.browse_metadata(object_id)
+    playbackitem = didl.objects[0]
+    assert len(didl.objects) == 1
+    playback_meta = didl.xml
+    dmr_protocol_info = await dmr.get_protocol_info()
+    resources = match_resources(playbackitem.res, dmr_protocol_info)
+    assert len(resources) > 0
+    play_resource = resources[0]
+    connection = prepare_for_connection(dmr, dms)
+    await dmr.av_transport.async_call_action('SetAVTransportURI',
+                                             InstanceID=connection.av_transport_id,
+                                             CurrentURI=play_resource.uri,
+                                             CurrentURIMetaData=playback_meta)
+    await dmr.av_transport.async_call_action('Play', InstanceID=connection.av_transport_id, Speed="1")
