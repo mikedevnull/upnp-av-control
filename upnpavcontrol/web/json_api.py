ffebd1c78bc74ea17a88d0ed1051ff61a6ccb4c1
Michael Meier <michael-meier@bluesheep.de>
2021-05-02 12:30:15 +0200

    add jsonapi helper to create response models for single objects

--- upnpavcontrol/web/json_api.py
+++ upnpavcontrol/web/json_api.py
@@ -28,3 +28,18 @@ def create_request_model(type_name: str, payload_model: Any):
                                 attributes=(payload_model, ...))
     RequestModel = create_model(f'Request[{type_name}]', data=(PayloadModel, ...))
     return RequestModel
+
+
+def create_response_model(type_name: str, payload_model: Any):
+    PayloadModel = create_model(f'ResponsePayload[{type_name}]',
+                                type=(Literal[type_name], type_name),
+                                id=(str, ...),
+                                attributes=(payload_model, ...))
+    ResponseModel = create_model(f'Response[{type_name}]', data=(PayloadModel, ...))
+
+    def factory(id: str, payload: payload_model):
+        data = {'id': id, 'attributes': payload}
+        return ResponseModel(data=data)
+
+    ResponseModel.create = staticmethod(factory)
+    return ResponseModel
