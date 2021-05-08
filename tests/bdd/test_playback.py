b0b94146d9b8d7f8e9c7c3f1d9af0107b4419ced
Michael Meier <michael-meier@bluesheep.de>
2021-05-08 15:43:59 +0200

    fixup web api paths in tests

--- tests/bdd/test_playback.py
+++ tests/bdd/test_playback.py
@@ -18,7 +18,7 @@ async def play_object_on_dmr(test_context, object_id, webclient):
     dms_device = test_context.get_device('FooMediaServer')
     dms_udn = dms_device.udn
     payload = {'data': {'type': 'playlistitem', 'attributes': {'dms': dms_udn, 'object_id': object_id}}}
-    response = await webclient.post(f'/player/{dmr_udn}/queue', json=payload)
+    response = await webclient.post(f'/api/player/{dmr_udn}/queue', json=payload)
     assert response.status_code == 201
 
 
@@ -37,7 +37,7 @@ async def check_playback_state_notification(test_context, dmr, state, event_bus_
 async def check_playback_state_with_api(test_context, dmr, state, webclient):
     assert state in ('PLAYING', 'STOPPED')
     dmr_device = test_context.get_device(dmr)
-    response = await webclient.get(f'/player/{dmr_device.udn}/playback')
+    response = await webclient.get(f'/api/player/{dmr_device.udn}/playback')
     assert response.status_code == 200
     data = response.json()['data']
     assert data['type'] == 'playbackinfo'
