31428fd1067203431327ad32ff23077574412212
Michael Meier <michael-meier@bluesheep.de>
2021-05-24 15:07:25 +0200

    make library browsing use json_api, including pagination support

---
+++ tests/bdd/test_media_server_api.py
@@ -0,0 +1,113 @@
+from pytest_bdd import scenarios, when, then, scenario, parsers
+from .async_utils import sync
+import logging
+from .common_steps import *  # noqa: F401, F403
+from .api_utils import get_browse_path, get_metadata_path, extract_browse_path
+
+_logger = logging.getLogger(__name__)
+
+
+@scenario("media_server_api.feature", "Pagination", example_converters=dict(first_element=int, last_element=int))
+def test_browse_pagination():
+    pass
+
+
+scenarios('media_server_api.feature')
+
+
+@when('the client requests the contents of the root element')
+@sync
+async def the_client_requests_the_contents_of_the_root_element(test_context, webclient):
+    """the client requests the contents of the root element."""
+    device_name = 'FooMediaServer'
+    device = test_context.get_device(device_name)
+    udn = device.udn
+    browse_uri = await get_browse_path(webclient, udn)
+    test_context.last_response = await webclient.get(browse_uri)
+    assert test_context.last_response.status_code == 200
+
+
+@when('the client requests link of the second item in the response')
+@sync
+async def the_client_requests_link_of_the_second_in_the_response(test_context, webclient):
+    """the client requests link of the second in the response."""
+    response_json = test_context.last_response.json()
+    second_item = response_json['data'][1]
+    browse_uri = extract_browse_path(second_item)
+    test_context.last_response = await webclient.get(browse_uri)
+
+
+@when('the client requests page <pagenumber> with size <pagesize> of object FakeLongArtistListing')
+@sync
+async def the_client_requests_paginated(test_context, webclient, pagenumber, pagesize):
+    """the client requests page <pagenumber> with size <pagesize> of object FakeLongArtistListing."""
+    device_name = 'FooMediaServer'
+    device = test_context.get_device(device_name)
+    udn = device.udn
+    browse_uri = await get_browse_path(webclient, udn)
+    pagination_params = {'page': pagenumber, 'pagesize': pagesize, 'objectID': 'FakeLongArtistListing'}
+    test_context.last_response = await webclient.get(browse_uri, query_string=pagination_params)
+
+
+@when('the client requests the object metadata of object SomeFakeObject on FooMediaServer')
+@sync
+async def the_client_requests_the_object_metadata_on_foomediaserver(test_context, webclient):
+    """the client requests the object metadata of object SomeFakeObject on FooMediaServer."""
+    device_name = 'FooMediaServer'
+    device = test_context.get_device(device_name)
+    udn = device.udn
+    browse_uri = await get_metadata_path(webclient, udn)
+    test_context.last_response = await webclient.get(browse_uri, query_string={'objectID': 'SomeFakeObject'})
+
+
+@then(parsers.cfparse('the response will contain elements from index <first_element> to index <last_element>'))
+def the_response_will_contain_elements_from_index_first_element_to_index_last_element(
+        test_context, first_element, last_element):
+    """the response will contain elements from index <first_element> to index <last_element>."""
+    assert test_context.last_response.status_code == 200
+    from .fake_upnp.foo_media_server import fakeLongArtistDidl
+    payload = test_context.last_response.json()
+    expected = fakeLongArtistDidl[first_element:last_element + 1]
+    assert len(payload['data']) == len(expected)
+    for expected_item, received_item in zip(expected, payload['data']):
+        assert received_item['id'] == expected_item.id
+        assert received_item['attributes']['title'] == expected_item.title
+        assert received_item['attributes']['album'] == expected_item.album
+        assert received_item['attributes']['artist'] == expected_item.artist
+
+
+@then('the response will contain the content listing of this second item')
+def the_response_will_contain_the_content_listing_of_this_second_item(test_context):
+    """the response will contain the content listing of this second item."""
+    from .fake_upnp.foo_media_server import fakeMusicDidl
+    assert test_context.last_response.status_code == 200
+    payload = test_context.last_response.json()
+    assert len(payload['data']) == len(fakeMusicDidl)
+    data0 = payload['data'][0]
+    assert data0['attributes']['title'] == fakeMusicDidl[0].title
+    assert data0['attributes']['album'] == fakeMusicDidl[0].album
+    assert data0['attributes']['artist'] == fakeMusicDidl[0].artist
+    assert data0['id'] == fakeMusicDidl[0].id
+
+    data1 = payload['data'][1]
+    assert data1['attributes']['title'] == fakeMusicDidl[1].title
+    assert data1['attributes']['album'] == fakeMusicDidl[1].album
+    assert data1['attributes']['artist'] == fakeMusicDidl[1].artist
+    assert data1['id'] == fakeMusicDidl[1].id
+
+
+@then('the response will contain the metadata of object SomeFakeObject on FooMediaServer')
+def the_response_will_contain_the_metadata_of_object_on_foomediaserver(test_context):
+    """the response will contain the metadata of object SomeFakeObject on FooMediaServer."""
+    from .fake_upnp.foo_media_server import someFakeObjectDidlMetadata
+    assert test_context.last_response.status_code == 200
+    payload = test_context.last_response.json()
+    _logger.debug(payload)
+    attributes = payload['data']['attributes']
+    assert attributes['id'] == someFakeObjectDidlMetadata.id
+    assert attributes['parentID'] == someFakeObjectDidlMetadata.parentID
+    assert attributes['upnpclass'] == someFakeObjectDidlMetadata.upnpclass
+    assert attributes['title'] == someFakeObjectDidlMetadata.title
+    assert attributes['album'] == someFakeObjectDidlMetadata.album
+    assert attributes['artist'] == someFakeObjectDidlMetadata.artist
+    assert attributes['originalTrackNumber'] == someFakeObjectDidlMetadata.originalTrackNumber
--- upnpavcontrol/web/api/library.py
+++ upnpavcontrol/web/api/library.py
@@ -1,58 +1,100 @@
 from .media_proxy import get_media_proxy_url
 from ...core.mediaserver import BrowseFlags
 from fastapi import APIRouter, Request, HTTPException
-from pydantic import BaseModel
-from typing import List
 import urllib.parse
 import logging
 import asyncio
+from .. import models
+from .. import json_api
 
 router = APIRouter()
 _logger = logging.getLogger(__name__)
 
+DevicesResponse = json_api.create_list_response_model('mediaserver', id_field='udn', PayloadModel=models.DeviceModel)
+DeviceResponse = json_api.create_response_model('mediaserver', models.DeviceModel)
 
-class MediaServer(BaseModel):
-    friendly_name: str
-    udn: str
-
-    class Config:
-        orm_mode = True
-
-
-def _fixup_media_server(x):
-    x.udn = urllib.parse.quote_plus(x.udn)
-    return x
+MetadataResponse = json_api.create_response_model('itemmetadata', models.LibraryItemMetadata)
+LibraryListingResponse = json_api.create_list_response_model('itemlist',
+                                                             id_field='id',
+                                                             PayloadModel=models.LibraryListItem)
 
 
-def _fixup_didl_item(item):
-    item.id = urllib.parse.quote_plus(item.id)
-    item.parentID = urllib.parse.quote_plus(item.parentID)
+def _fixup_item_media_urls(item):
     albumArtURI = getattr(item, 'albumArtURI', None)
     if albumArtURI is not None:
-        item.albumArtURI = get_media_proxy_url(albumArtURI)
+        item.albumArtURI = get_media_proxy_url(albumArtURI.uri)
     artistDiscographyURI = getattr(item, 'artistDiscographyURI', None)
     if artistDiscographyURI is not None:
-        item.artistDiscographyURI = get_media_proxy_url(artistDiscographyURI)
+        item.artistDiscographyURI = get_media_proxy_url(artistDiscographyURI.uri)
     return item
 
 
 def _fixup_didl_items(items):
-    return (_fixup_didl_item(x) for x in items)
+    return (_fixup_item_media_urls(x) for x in items)
 
 
-@router.get('/devices', response_model=List[MediaServer])
+@router.get('/', response_model=DevicesResponse)
 def get_media_library_devices(request: Request):
-    return [_fixup_media_server(MediaServer.from_orm(x)) for x in request.app.av_control_point.mediaservers]
+    def create_links(d: models.DeviceModel):
+        return {'self': request.url_for('get_media_library_device', udn=d.udn)}
+
+    resp = DevicesResponse.create(request.app.av_control_point.mediaservers, links_factory=create_links)
+    return resp
+
+
+@router.get('/{udn}', response_model=DeviceResponse)
+def get_media_library_device(request: Request, udn: str):
+    device = request.app.av_control_point.get_mediaserver_by_UDN(udn)
+    relationships = {
+        'browse': {
+            'links': {
+                'related': request.url_for('browse_library', udn=udn)
+            }
+        },
+        'metadata': {
+            'links': {
+                'related': request.url_for('get_object_metadata', udn=udn)
+            }
+        }
+    }
+    return DeviceResponse.create(device.udn, device, request.url_for('get_media_library_device', udn=udn),
+                                 relationships)
+
+
+def _url_for_query(request, path, params, query):
+    url = request.url_for(path, **params)
+    parsed = list(urllib.parse.urlparse(url))
+    parsed[4] = urllib.parse.urlencode(query)
+    return urllib.parse.urlunparse(parsed)
+
+
+def _browse_list_relationship_factory(request, udn, item):
+    relationships = {
+        'browse': {
+            'links': {
+                'related': _url_for_query(request, 'browse_library', {'udn': udn}, {'objectID': item.id})
+            }
+        },
+        'metadata': {
+            'links': {
+                'related': _url_for_query(request, 'get_object_metadata', {'udn': udn}, {'objectID': item.id})
+            }
+        }
+    }
+    return relationships
 
 
 @router.get('/{udn}/browse')
-async def browse_library(request: Request, udn: str, objectID: str = '0'):
+async def browse_library(request: Request, udn: str, objectID: str = '0', page: int = 0, pagesize: int = 0):
     try:
         udn = urllib.parse.unquote_plus(udn)
         objectID = urllib.parse.unquote_plus(objectID)
         server = request.app.av_control_point.get_mediaserver_by_UDN(udn)
-        result = await server.browse(objectID)
-        return _fixup_didl_items(result)
+        result = await server.browse(objectID, starting_index=page * pagesize, requested_count=pagesize)
+
+        payload = [models.LibraryItemMetadata.from_orm(_fixup_item_media_urls(x)) for x in result.objects]
+        return LibraryListingResponse.create(
+            payload, relationship_factory=lambda x: _browse_list_relationship_factory(request, udn, x))
     except asyncio.TimeoutError:
         _logger.error('Mediaserver browse request timed out')
         raise HTTPException(status_code=504, detail="Request to mediaserver timed out")
@@ -61,14 +103,15 @@ async def browse_library(request: Request, udn: str, objectID: str = '0'):
         raise HTTPException(status_code=404)
 
 
-@router.get('/{udn}/metadata')
+@router.get('/{udn}/metadata', response_model_exclude_unset=True)
 async def get_object_metadata(request: Request, udn: str, objectID: str = '0'):
     try:
         udn = urllib.parse.unquote_plus(udn)
         objectID = urllib.parse.unquote_plus(objectID)
         server = request.app.av_control_point.get_mediaserver_by_UDN(udn)
         result = await server.browse(objectID, browse_flag=BrowseFlags.BrowseMetadata)
-        return _fixup_didl_items(result)
+        payload = models.LibraryItemMetadata.from_orm(_fixup_item_media_urls(result.objects[0]))
+        return MetadataResponse.create(objectID, payload)
     except asyncio.TimeoutError:
         _logger.error('Mediaserver metadata request timed out')
         raise HTTPException(status_code=504, detail="Request to mediaserver timed out")
