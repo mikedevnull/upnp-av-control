c2f356782dd045ca38b2e9acfd436a7ae0f22715
Michael Meier <michael-meier@bluesheep.de>
2021-07-18 13:43:31 +0200

    config(frontend): use source map for client in dev mode

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -61,6 +61,12 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    extend(config, ctx) {
+      if (ctx.isDev) {
+        config.devtool = ctx.isClient ? 'source-map' : 'inline-source-map'
+      }
+    },
+  },
   server: { host: '0' },
 }
