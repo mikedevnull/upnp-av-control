99e9800524e0ea3dd4c6f4df7f5b8c0a58ba1290
Michael Meier <michael-meier@bluesheep.de>
2021-07-18 13:47:46 +0200

    (frontend): minimal library browsing and play action for single items

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,63 +1,67 @@
 <template>
   <div class="h-screen flex flex-col">
     <TopBar
-      ><template #nav-action><NavIconDown class="w-6 h-6" /></template> Now
-      playing
-      <template #extra-action
-        ><nuxt-link to="select-player"
-          ><DevicesIcon class="w-6 h-6" /></nuxt-link></template
-    ></TopBar>
-    <player class="pt-4" @openPlayerSelection="$router.push('select-player')" />
+      ><template #nav-action
+        ><a href="#" @click="browseBack"> <NavIconBack class="w-6 h-6" /></a
+      ></template>
+      Browse
+    </TopBar>
+    <div class="flex-grow m-4">
+      <h1 class="text-4xl mb-4">{{ title }}</h1>
+      <ul>
+        <li v-for="child in children" :key="child.id">
+          <library-list-item :item="child" @browse="browse" />
+        </li>
+      </ul>
+    </div>
+    <MiniPlayer />
   </div>
 </template>
 
 <script>
-import NavIconDown from '~/assets/nav-down.svg?inline'
-import DevicesIcon from '~/assets/control-devices.svg?inline'
+import NavIconBack from '~/assets/nav-back.svg?inline'
+
 export default {
-  components: { NavIconDown, DevicesIcon },
+  components: { NavIconBack },
   data() {
-    return {}
+    return {
+      currentId: undefined,
+      entry: undefined,
+      children: [],
+    }
   },
 
-  methods: {},
+  async fetch() {
+    this.entry = await this.$upnpapi.getLibraryItem(this.currentId)
+    this.children = await this.$upnpapi.browseLibrary(this.currentId)
+  },
+
+  computed: {
+    title() {
+      if (this.entry && this.entry.title) {
+        return this.entry.title
+      }
+      return ''
+    },
+  },
+  methods: {
+    browse(child) {
+      this.currentId = child.id
+      this.entry = undefined
+      this.children = []
+      this.$fetch()
+    },
+    browseBack() {
+      this.currentId = this.entry.parentID
+      if (this.currentId === '-1') {
+        this.currentId = undefined
+      }
+      this.children = []
+      this.entry = undefined
+      this.$fetch()
+    },
+  },
 }
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
