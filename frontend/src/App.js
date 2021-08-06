e88a2ef25555b31afec35b052a6d76dcc3d4b354
Michael Meier <michael-meier@bluesheep.de>
2021-08-06 10:47:19 +0200

    first routing / page changes between library browser and player view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
 import { LibraryBrowser } from "./pages";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Player } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <LibraryBrowser />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/player">
+            <Player />
+          </Route>
+          <Route path="/">
+            <LibraryBrowser />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/components/miniplayer.tsx
+++ frontend/src/components/miniplayer.tsx
@@ -1,11 +1,15 @@
 import { ReactComponent as NavUp } from "../assets/nav-up.svg";
 import { ReactComponent as NextIcon } from "../assets/control-next.svg";
 import { ReactComponent as PlayIcon } from "../assets/control-play-outline.svg";
+import { Link } from "react-router-dom";
 
 export default function Miniplayer() {
   return (
     <div className="p-4 h-16 flex items-center border-t">
-      <NavUp />
+      <Link to="/player">
+        <NavUp />
+      </Link>
+
       <img className="mx-2 border h-12" src="logo192.png"></img>
       <span className="m-2 text-left flex-grow">Miniplayer</span>
       <span>0:00</span>
