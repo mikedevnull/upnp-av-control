20be9797461b53ef8f57c59f18212bf39b5e3777
Michael Meier <michael-meier@bluesheep.de>
2021-08-13 00:05:44 +0200

    test(frontend): fully change playback control tests to typescript

--- frontend/src/upnpapi/playback_control.spec.ts
+++ frontend/src/upnpapi/playback_control.spec.ts
@@ -1,9 +1,12 @@
 import * as api from "./player";
 import PlaybackControl from "./playback_control";
 import { PlaybackControlEvent } from "./playback_control";
-import MockEventBus from "./event_bus";
+import EventBus from "./event_bus";
 jest.mock("./event_bus");
 jest.mock("./player");
+
+const MockEventBus = <jest.Mock<EventBus>>EventBus;
+const mockedGetDevice = api.getDevices as jest.Mock;
 
 const devices = [
   { id: "1234", name: "foo" },
@@ -12,17 +15,17 @@ const devices = [
 const flushPromises = () => new Promise(setImmediate);
 
 describe("PlaybackControl", () => {
-  let eventBus;
+  let eventBus: any;
 
   beforeAll(() => {});
 
   beforeEach(() => {
     eventBus = new MockEventBus();
-    jest.clearAllMocks();
+    mockedGetDevice.mockClear();
   });
 
   it("provides a list of available devices", async () => {
-    api.getDevices.mockReturnValueOnce(devices);
+    mockedGetDevice.mockReturnValueOnce(devices);
 
     const control = new PlaybackControl(eventBus);
     await flushPromises();
@@ -32,12 +35,13 @@ describe("PlaybackControl", () => {
   });
 
   it("emits an event when a new playback devices becomes available", async () => {
-    api.getDevices.mockReturnValue([]);
+    mockedGetDevice.mockReturnValueOnce(devices);
     const control = new PlaybackControl(eventBus);
+    await flushPromises();
 
     const deviceCb = jest.fn();
     control.on(PlaybackControlEvent.DEVICES_CHANGED, deviceCb);
-    api.getDevices.mockReturnValueOnce([{ id: "abc", name: "Baz" }]);
+    mockedGetDevice.mockReturnValueOnce([{ id: "abc", name: "Baz" }]);
 
     eventBus.triggerNewDevice({ udn: "abc", deviceType: "renderer" });
     await flushPromises();
@@ -45,13 +49,13 @@ describe("PlaybackControl", () => {
   });
 
   it("emits an event when a playback devices gets lost", async () => {
-    api.getDevices.mockReturnValue([{ id: "abc", name: "Baz" }]);
+    mockedGetDevice.mockReturnValue([{ id: "abc", name: "Baz" }]);
     const control = new PlaybackControl(eventBus);
     await flushPromises();
 
     const deviceCb = jest.fn();
     control.on(PlaybackControlEvent.DEVICES_CHANGED, deviceCb);
-    api.getDevices.mockReturnValueOnce([]);
+    mockedGetDevice.mockReturnValueOnce([]);
 
     eventBus.triggerDeviceLost({ udn: "abc", deviceType: "renderer" });
     await flushPromises();
@@ -59,7 +63,7 @@ describe("PlaybackControl", () => {
   });
 
   it("emits an event when selecting an available playback device", async () => {
-    api.getDevices.mockReturnValueOnce(devices);
+    mockedGetDevice.mockReturnValueOnce(devices);
     const control = new PlaybackControl(eventBus);
 
     const playerCb = jest.fn();
@@ -74,7 +78,7 @@ describe("PlaybackControl", () => {
   });
 
   it("emits an event when a pre-selected playback device becomes available", async () => {
-    api.getDevices.mockReturnValueOnce([]);
+    mockedGetDevice.mockReturnValueOnce([]);
     const control = new PlaybackControl(eventBus);
 
     const playerCb = jest.fn();
@@ -83,7 +87,7 @@ describe("PlaybackControl", () => {
     control.selectedPlayerId = "1234";
     expect(playerCb).not.toHaveBeenCalled();
 
-    api.getDevices.mockReturnValueOnce(devices);
+    mockedGetDevice.mockReturnValueOnce(devices);
     eventBus.triggerNewDevice({ udn: "1234", deviceType: "renderer" });
     await flushPromises();
 
@@ -92,7 +96,7 @@ describe("PlaybackControl", () => {
   });
 
   it("emits an event when the selected playback device gets lost", async () => {
-    api.getDevices.mockReturnValueOnce(devices);
+    mockedGetDevice.mockReturnValueOnce(devices);
     const control = new PlaybackControl(eventBus);
 
     const playerCb = jest.fn();
@@ -102,7 +106,7 @@ describe("PlaybackControl", () => {
     expect(control.isPlayerPresent).toBeTruthy();
 
     eventBus.triggerDeviceLost({ udn: "1234", deviceType: "renderer" });
-    api.getDevices.mockReturnValueOnce([]);
+    mockedGetDevice.mockReturnValueOnce([]);
     await flushPromises();
 
     expect(playerCb).toHaveBeenCalledTimes(1);
@@ -110,7 +114,7 @@ describe("PlaybackControl", () => {
   });
 
   it("emits an event when active player is set to undefined", async () => {
-    api.getDevices.mockReturnValueOnce(devices);
+    mockedGetDevice.mockReturnValueOnce(devices);
     const control = new PlaybackControl(eventBus);
 
     const playerCb = jest.fn();
