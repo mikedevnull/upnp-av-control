764dd15e3c7accddef7f1f9737019abde30a1c5c
Michael Meier <michael-meier@bluesheep.de>
2021-08-15 18:57:45 +0200

    feat(frontend): first primitive library browsing implementation

--- frontend/src/components/browser.tsx
+++ frontend/src/components/browser.tsx
@@ -1,3 +1,37 @@
-export default function Browser() {
-  return <div className="flex-grow">Browser</div>;
+import { useEffect } from "react";
+import { useState } from "react";
+import * as library from "../upnpapi/library";
+import { LibraryListItem } from "../upnpapi/types";
+import { BrowseListItem as ListItemComponent } from "./browse-list-item";
+
+interface BrowserProps {
+  id?: string;
+  clickHandler?: CallableFunction;
+}
+
+export default function Browser({ id, clickHandler }: BrowserProps) {
+  const [items, setItems] = useState<LibraryListItem[]>([]);
+
+  useEffect(() => {
+    setItems([]);
+    library.browse(id).then(setItems);
+  }, [id]);
+  const defaultActionHandler = (item: LibraryListItem) => {
+    console.log(item.upnpclass);
+    if (item.upnpclass.startsWith("container") && clickHandler) {
+      clickHandler(item);
+    }
+  };
+  const content = items.map((e) => (
+    <ListItemComponent
+      item={e}
+      key={e.id}
+      clickHandler={defaultActionHandler}
+    />
+  ));
+  return (
+    <div className="flex-grow">
+      <ul>{content}</ul>
+    </div>
+  );
 }
