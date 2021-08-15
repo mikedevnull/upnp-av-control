2fe6f7722688693e000a75723e3c93fcf152c79f
Michael Meier <michael-meier@bluesheep.de>
2021-08-15 18:56:22 +0200

    feat(frontend): simplify upnp api browse function and fix return types

--- frontend/src/upnpapi/library.ts
+++ frontend/src/upnpapi/library.ts
@@ -1,24 +1,19 @@
 import { LibraryListItem } from "./types";
 import { adaptTo } from "./utils";
 
-export function browse(id: string) {
+export function browse(id?: string) {
+  let url = `/api/library/`;
   if (id) {
-    const url = `/api/library/${id}`;
-    return fetch(url)
-      .then((response: any) => response.json())
-      .then((data: any) => adaptTo<LibraryListItem[]>(data));
-  } else {
-    const url = `/api/library/`;
-    return fetch(url)
-      .then((response: any) => response.json())
-      .then((data: any) => adaptTo<LibraryListItem[]>(data));
+    url = `/api/library/${id}`;
   }
+  return fetch(url)
+    .then((response: any) => response.json())
+    .then((data: any) => adaptTo<LibraryListItem[]>(data));
 }
 
 export function getItem(id: string) {
-  if (id) {
-    const url = `/api/library/${id}/metadata`;
-    return fetch(url).then((response: any) => response.json());
-  }
-  return {};
+  const url = `/api/library/${id}/metadata`;
+  return fetch(url)
+    .then((response: any) => response.json())
+    .then((data: any) => adaptTo<LibraryListItem>(data));
 }
