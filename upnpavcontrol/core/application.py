176f8e70cd1860d1d317af328ab8271fcd5909a3
Michael Meier <michael-meier@bluesheep.de>
2022-01-03 11:40:48 +0100

    add persistent playback controller to av control point

--- upnpavcontrol/core/application.py
+++ upnpavcontrol/core/application.py
@@ -3,6 +3,8 @@ from upnpavcontrol.core.discovery.registry import MediaDeviceType
 from .discovery import DeviceRegistry, DeviceEntry, DiscoveryEventType
 from .mediarenderer import create_media_renderer, MediaRenderer
 from .mediaserver import MediaServer
+from .playback.controller import PlaybackController
+from .playback.utils import PlaybackControllableWrapper
 from .notification_backend import NotificationBackend, AiohttpNotificationEndpoint
 from async_upnp_client.aiohttp import AiohttpRequester
 from async_upnp_client import UpnpFactory, UpnpDevice
@@ -30,6 +32,7 @@ class AVControlPoint(object):
         self._device_discover_observer = Observable[MediaDeviceDiscoveryEvent]()
         self._renderers: Dict[str, MediaRenderer] = {}
         self._servers: Dict[str, MediaServer] = {}
+        self._playback_controller: Dict[str, PlaybackController] = {}
         if device_factory is None:
             self._upnp_device_factory = UpnpFactory(AiohttpRequester())
         else:
@@ -59,6 +62,9 @@ class AVControlPoint(object):
     def get_mediarenderer_by_UDN(self, udn: str) -> MediaRenderer:
         return self._renderers[udn]
 
+    def get_controller_for_renderer(self, udn: str) -> PlaybackController:
+        return self._playback_controller[udn]
+
     def on_device_discovery_event(self, callback: Callable[[MediaDeviceDiscoveryEvent], Awaitable[None]]):
         return self._device_discover_observer.subscribe(callback)
 
@@ -76,15 +82,15 @@ class AVControlPoint(object):
             if event_type == DiscoveryEventType.NEW_DEVICE:
                 device = await self._create_device(entry)
                 if entry.device_type == MediaDeviceType.MEDIASERVER:
-                    self._servers[entry.udn] = cast(MediaServer, device)
+                    self._add_new_server(entry.udn, cast(MediaServer, device))
                 elif entry.device_type == MediaDeviceType.MEDIARENDERER:
-                    self._renderers[entry.udn] = cast(MediaRenderer, device)
+                    await self._add_new_renderer(entry.udn, cast(MediaRenderer, device))
                 await self._notify_discovery(event)
             elif event_type == DiscoveryEventType.DEVICE_LOST:
                 if entry.device_type == MediaDeviceType.MEDIASERVER:
-                    device = self._servers.pop(entry.udn)
+                    self._remove_lost_server(entry.udn)
                 elif entry.device_type == MediaDeviceType.MEDIARENDERER:
-                    device = self._renderers.pop(entry.udn)
+                    self._remove_lost_renderer(entry.udn)
                 await self._notify_discovery(event)
         except Exception:
             _logger.exception('Failed to handle device discovery event')
@@ -100,3 +106,19 @@ class AVControlPoint(object):
         elif entry.device_type == MediaDeviceType.MEDIARENDERER:
             return await create_media_renderer(raw_device, self._notify_receiver)
         raise RuntimeError('Cannot create device, is neither Renderer nor Server')
+
+    async def _add_new_renderer(self, udn: str, device: MediaRenderer):
+        self._renderers[udn] = device
+        self._playback_controller[udn] = PlaybackController()
+        wrapped_device = PlaybackControllableWrapper(device, self.get_mediaserver_by_UDN)
+        await self._playback_controller[udn].setup_player(wrapped_device)
+
+    def _remove_lost_renderer(self, udn: str):
+        self._renderers.pop(udn)
+        self._playback_controller.pop(udn)
+
+    def _add_new_server(self, udn: str, device: MediaServer):
+        self._servers[udn] = device
+
+    def _remove_lost_server(self, udn: str):
+        self._servers.pop(udn)
