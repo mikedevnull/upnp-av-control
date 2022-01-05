44587c5accd996a76758303c7a61d9040a2f35a2
Michael Meier <michael-meier@bluesheep.de>
2022-01-05 11:33:22 +0100

    change playback queue interface to list without item removal

--- upnpavcontrol/core/playback/queue.py
+++ upnpavcontrol/core/playback/queue.py
@@ -13,18 +13,39 @@ class PlaybackItem():
 class PlaybackQueue():
     def __init__(self):
         self._items = []
+        self._current_item_index: typing.Optional[PlaybackItem] = None
 
     @property
     def items(self):
         return self._items
 
-    def insert(self, dms, object_id, title):
+    @property
+    def current_item(self):
+        if self._current_item_index is None:
+            return None
+        return self._items[self._current_item_index]
+
+    @property
+    def current_item_index(self):
+        return self._current_item_index
+
+    def append(self, dms, object_id, title):
         self._items.append(PlaybackItem(dms=dms, object_id=object_id, title=title))
 
     def clear(self):
         self._items.clear()
+        self._current_item_index = None
 
     def next_item(self):
-        if len(self._items) > 0:
-            return self._items.pop(0)
-        return None
+        if len(self._items) == 0:
+            return None
+        if self._current_item_index is None:
+            self._current_item_index = 0
+        else:
+            self._current_item_index += 1
+
+        if self._current_item_index >= len(self._items):
+            self._current_item_index = None
+            return None
+
+        return self._items[self._current_item_index]
