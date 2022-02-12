412e7c5eb53d4a404b5000a13e3fae4ca42ac9de
Michael Meier <michael-meier@bluesheep.de>
2022-02-13 00:26:38 +0100

    test(backend): jsonrpc match reply by id discarding other messages

--- tests/bdd/json_rpc_connection.py
+++ tests/bdd/json_rpc_connection.py
@@ -1,8 +1,10 @@
 from upnpavcontrol.web import json_rpc
 import asyncio
+import pydantic
 
 
 class JsonRPCTestConnection(object):
+
     def __init__(self, websocket):
         self._websocket = websocket
         self._nextId = 1
@@ -24,14 +26,19 @@ class JsonRPCTestConnection(object):
         request = json_rpc.JsonRPCRequest(id=self._nextId, method=method, params=params, jsonrpc='2.0')
         self._nextId = +1
         await self._websocket.send_text(request.json())
-        raw_reply = await asyncio.wait_for(self._websocket.receive_json(), timeout=self.timeout)
-        reply = json_rpc.JsonRPCResponse(**raw_reply)
-        assert reply.id == request.id
-        return reply.result
+        while True:
+            try:
+                raw_reply = await asyncio.wait_for(self._websocket.receive_json(), timeout=self.timeout)
+                reply = json_rpc.JsonRPCResponse(**raw_reply)
+                if reply.id == request.id:
+                    return reply.result
+            except pydantic.ValidationError:
+                pass
 
     async def wait_for_notification(self):
         raw_reply = await asyncio.wait_for(self._websocket.receive_json(), timeout=self.timeout)
         reply = json_rpc.JsonRPCNotification(**raw_reply)
+
         return reply
 
     async def clear_pending_notifications(self):
