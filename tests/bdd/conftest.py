a91cf3e63f80eb9f9bf8e6550b4572248ccf9508
Michael Meier <michael-meier@bluesheep.de>
2022-02-12 22:12:10 +0100

    chore(backend): add changes for future pytest_asyncio strict mode requirements

--- tests/bdd/conftest.py
+++ tests/bdd/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import pytest_asyncio
 from starlette.websockets import WebSocketDisconnect
 import upnpavcontrol.core.discovery
 from async_asgi_testclient import TestClient
@@ -7,7 +8,7 @@ from .json_rpc_connection import JsonRPCTestConnection
 
 
 @pytest.mark.asyncio
-@pytest.fixture
+@pytest_asyncio.fixture
 def test_context(monkeypatch, event_loop):
     context = TestContext()
     monkeypatch.setattr(upnpavcontrol.core.discovery.scan, 'async_search', context.fake_async_search)
@@ -16,7 +17,7 @@ def test_context(monkeypatch, event_loop):
 
 
 @pytest.mark.asyncio
-@pytest.fixture
+@pytest_asyncio.fixture
 async def webclient(test_context, event_loop):
     from upnpavcontrol.web import application
     application.app.av_control_point = test_context.control_point
@@ -25,7 +26,7 @@ async def webclient(test_context, event_loop):
 
 
 @pytest.mark.asyncio
-@pytest.fixture
+@pytest_asyncio.fixture
 async def event_bus_connection(webclient, event_loop):
     try:
         async with webclient.websocket_connect('/api/ws/events') as websocket:
