2016f12dfd1a0b4b5c3623e4f29a1ac5545fff66
Michael Meier <michael-meier@bluesheep.de>
2022-02-19 19:37:01 +0100

    add backend connection state to playback control

--- frontend/src/upnpapi/__mocks__/event_bus.ts
+++ frontend/src/upnpapi/__mocks__/event_bus.ts
@@ -1,20 +1,39 @@
 import _ from "lodash";
+import {
+  NewDeviceMessage,
+  DeviceLostMessage,
+  PlaybackInfoMessage,
+  ControlPointState,
+} from "../event_bus";
 
 export default class MockEventBus {
-  onNewDevice: { (message: any): void } | undefined;
-  onDeviceLost: { (message: any): void } | undefined;
+  onNewDevice?: (message: NewDeviceMessage) => void;
+  onDeviceLost?: (message: DeviceLostMessage) => void;
+  onClosed?: () => void;
+  private _state: ControlPointState = "connected";
+
   private infoSubscriptions = new Array<String>();
 
-  triggerDeviceLost(data: { udn: string; deviceType: string }) {
+  triggerDeviceLost(data: DeviceLostMessage) {
     if (this.onDeviceLost) {
       this.onDeviceLost(data);
     }
   }
-  triggerNewDevice(data: { udn: string; deviceType: string }) {
+  triggerNewDevice(data: NewDeviceMessage) {
     if (this.onNewDevice) {
       this.onNewDevice(data);
     }
   }
+  triggerClosed() {
+    this._state = "closed";
+    if (this.onClosed) {
+      this.onClosed();
+    }
+  }
+  get state() {
+    return this._state;
+  }
+
   subscribePlaybackInfo(playerId: string) {
     this.infoSubscriptions.push(playerId);
   }
