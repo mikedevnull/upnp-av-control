ab0e9da36e7671617b52790686cca51305bbe2e4
Michael Meier <michael-meier@bluesheep.de>
2022-02-20 01:12:07 +0100

    test(frontend): first tests for player component

--- frontend/src/upnpapi/__mocks__/playback_control.ts
+++ frontend/src/upnpapi/__mocks__/playback_control.ts
@@ -3,8 +3,17 @@ import { PlaybackControlState } from "../playback_control";
 
 export default class MockPlaybackControl extends EventEmitter {
   playItemsImmediatly = jest.fn();
+  playPause = jest.fn();
+  setVolume = jest.fn();
+
   fakeBackendState = jest.fn().mockReturnValue("connected");
+  fakePlayerPresent = jest.fn().mockReturnValue(true);
+
   get backendState(): PlaybackControlState {
     return this.fakeBackendState();
+  }
+
+  get isPlayerPresent() {
+    return this.fakePlayerPresent();
   }
 }
