1f316a43a87b709f452b39eedad5c9e6bf896417
Michael Meier <michael-meier@bluesheep.de>
2022-05-01 22:36:02 +0200

    Remove overlay on hover that prevents first touch event on ios
    While there are workarounds to get this working with the overlay, the click-to-play-overlay-indicator itself is not the best design idea, there should be other ways to solve this
    
    Fixes #162

--- frontend/src/components/browse-list-item.tsx
+++ frontend/src/components/browse-list-item.tsx
@@ -12,12 +12,6 @@ export function BrowseListItem({ item, clickHandler }: BrowseListItemProps) {
     "block group flex items-center px-4 h-16 hover:bg-primary-lightest cursor-pointer text-left";
   const placeholderIcon = item.upnpclass === "item" ? IconTrack : IconContainer;
   const img = item.image ? item.image : placeholderIcon;
-  const overlay =
-    item.upnpclass === "item" ? (
-      <div className="flex justify-center items-center absolute opacity-0 group-hover:opacity-70 m-1 rounded-xl h-14 w-14 bg-gray-800 text-white font-semibold">
-        <img src={PlayIcon} className="h-10 w-10" />
-      </div>
-    ) : null;
   return (
     <li
       onClick={() => {
@@ -32,7 +26,6 @@ export function BrowseListItem({ item, clickHandler }: BrowseListItemProps) {
         alt="album art"
         className="h-14 w-14 m-1 rounded-xl object-scale-down"
       />
-      {overlay}
       {item.title}
     </li>
   );
