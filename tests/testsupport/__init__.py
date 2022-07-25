b282d30ec92048267998e37ded6fa526c2944381
Michael Meier <michael-meier@bluesheep.de>
2022-07-25 17:15:11 +0200

    chore(backend): remove old discovery code no longer in use

--- tests/testsupport/__init__.py
+++ tests/testsupport/__init__.py
@@ -1,6 +1,5 @@
 from .asyncmock_stub import AsyncMock
-from .null_advertisement_listener import NullAdvertisementListener
 from .upnp_test_requester import UpnpTestRequester
 from .notification_test_endpoint import NotificationTestEndpoint
 
-__all__ = ['AsyncMock', 'NullAdvertisementListener', 'UpnpTestRequester', 'NotificationTestEndpoint']
+__all__ = ['AsyncMock', 'UpnpTestRequester', 'NotificationTestEndpoint']
